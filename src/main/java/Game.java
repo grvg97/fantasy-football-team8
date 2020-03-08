9f0bcf5f1f08f5964f4b17a8f6c93d99bc8f3ecb
SunnyDOTT <s.a.dairam@student.vu.nl>
2020-03-08 21:31:57 +0100

    Assignment2-Implementation working fat JAR.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,25 +3,40 @@ import com.google.gson.Gson;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.*;
+import java.util.Scanner;
 
 /** This Program implements feature 2
  *  F2: CREATE TEAMS: Users shall be able to one or more teams choosing players from different teams and/or leagues.
  **/
-class Game {
-    static Scanner scanner = new Scanner(System.in);
-    static UserDatabase userDatabase;
-    static LeagueDatabase leagueDatabase;
-    static MarketPlace marketPlace;
+public class Game {
+    private Scanner scanner = new Scanner(System.in);
+    private UserDatabase userDatabase;
+    private LeagueDatabase leagueDatabase;
+    private MarketPlace marketPlace;
 
+    Game() throws IOException {
+        this.marketPlace = new Gson().fromJson(this.getResponseBody("https://fantasy.premierleague.com/api/bootstrap-static/"), MarketPlace.class);
+        String userFileName = "DatabaseUser.json";
+        String leagueFileName = "DatabasueLeague.json";
+        try {
+            userDatabase = new Gson().fromJson(new FileReader(userFileName), UserDatabase.class);
+            leagueDatabase = new Gson().fromJson(new FileReader(leagueFileName), LeagueDatabase.class);
+        } catch (Exception e) {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(userFileName));
+            writer.close();
+            writer = new BufferedWriter(new FileWriter(leagueFileName));
+            writer.close();
 
-    public static MarketPlace parse(String responseBody) {
-        MarketPlace marketPlace = new Gson().fromJson(responseBody, MarketPlace.class);
-        return marketPlace;
+            this.userDatabase = new UserDatabase();
+            this.leagueDatabase = new LeagueDatabase();
+            League globalLeague = new League("GlobalLeague");
+            this.leagueDatabase.addLeague(globalLeague);
+        }
     }
 
+
     // Implemented for bonus
-    public static String getResponseBody(String apiURL) throws IOException {
+    private String getResponseBody(String apiURL) throws IOException {
         BufferedReader reader;
         String line;
         StringBuffer responseBody = new StringBuffer();
@@ -37,78 +52,68 @@ class Game {
         return responseBody.toString();
     }
 
-    private static void choosePlayers(MarketPlace market, User user, int amount, int position, Scanner scanner) {
-        market.showPlayersWithPosition(position);
+    private void choosePlayers(User user, int amount, int position) {
+        this.marketPlace.showPlayersWithPosition(position);
         System.out.println("Choose the players at position " + position + ". Choose exactly " + amount + " players.");
         for(int i = 0; i < amount; i++) {
-            user.buyPlayer(market.getPlayer(scanner.nextInt()));
+            user.buyPlayer(this.marketPlace.getPlayer(this.scanner.nextInt()));
         }
     }
 
-    public static void constructTeam(MarketPlace marketPlace, User user) {
-        Scanner scanner = new Scanner(System.in);
+    public void constructTeam(User user) {
         int DEF = 4; int MID = 3; int FOR = 3; // Formation: 4-3-3
         int BENCH = 4; int GK = 1;
 
         // Select 1 goalkeeper
-        choosePlayers(marketPlace, user, GK, 1, scanner);
+        choosePlayers(user, GK, 1);
 
         // Select 4 defenders
-        choosePlayers(marketPlace, user, DEF, 2, scanner);
+        choosePlayers(user, DEF, 2);
 
         // Select 4 midfielders
-        choosePlayers(marketPlace, user, MID, 3, scanner);
+        choosePlayers(user, MID, 3);
 
         // Select 3 Forwards
-        choosePlayers(marketPlace, user, FOR, 4, scanner);
+        choosePlayers(user, FOR, 4);
 
-        marketPlace.showPlayers();
+        this.marketPlace.showPlayers();
         System.out.println("Next up are the bench. Choose exactly 4 players from any position.");
         for(int i = 0; i < BENCH; i++) {
-            user.buyPlayer(marketPlace.getPlayer(scanner.nextInt()));
+            user.buyPlayer(this.marketPlace.getPlayer(this.scanner.nextInt()));
         }
     }
 
-    public static User authenticateUser() throws IOException {
-        userDatabase = new Gson().fromJson(new FileReader("src/database/DatabaseUser.json"), UserDatabase.class);
-        leagueDatabase = new Gson().fromJson(new FileReader("src/database/DatabaseLeague.json"), LeagueDatabase.class);
-        if(userDatabase == null)
-            userDatabase = new UserDatabase();
-
+    public User authenticateUser() throws IOException {
         System.out.println("Welcome to Fantasy Football!");
         System.out.println("Please enter a username: ");
-        String username = scanner.next();
+        String username = this.scanner.next();
         System.out.println("Please enter a password: ");
-        String password = scanner.next();
+        String password = this.scanner.next();
 
-        User user = userDatabase.authUser(username, password);
+        User user = this.userDatabase.authUser(username, password);
         if(user != null) {
             System.out.println("You're logged in!");
         } else {
-            user = userDatabase.addUser(new User(username, password));
+            user = this.userDatabase.addUser(new User(username, password));
             System.out.print("Registration complete!\n");
-            }
+        }
 
         return user;
     }
 
-    public static void beginTutorial(User user) {
+    public void beginTutorial(User user) {
         // Creation of team start of the game
         System.out.println("Please give your team a name: ");
-        user.createTeam(scanner.next());
+        user.createTeam(this.scanner.next());
 
         // Select players and add them to the team
-        constructTeam(marketPlace, user);
+        constructTeam(user);
 
-        League globalLeague = leagueDatabase.getGlobalLeague();
+        League globalLeague = this.leagueDatabase.getGlobalLeague();
         globalLeague.addUser(user);
     }
 
-    public static void start(User user) throws IOException, InterruptedException {
-        // Get data from api and parse the data to use it properly
-        String responseBody = getResponseBody("https://fantasy.premierleague.com/api/bootstrap-static/");
-        marketPlace = parse(responseBody);
-
+    public void start(User user) throws IOException, InterruptedException {
         if(!user.hasTeam())
             beginTutorial(user);
 
@@ -116,19 +121,19 @@ class Game {
         user.displayTeam();
         System.out.println("\n");
 
-        League globalLeague = leagueDatabase.getGlobalLeague();
+        League globalLeague = this.leagueDatabase.getGlobalLeague();
         globalLeague.showRanking();
         end(user);
     }
 
-    public static void end(User user) throws IOException {
+    public void end(User user) throws IOException {
         // At exit save userDB
-        BufferedWriter writer = new BufferedWriter(new FileWriter("src/database/DatabaseUser.json"));
-        writer.write(new Gson().toJson(userDatabase));
+        BufferedWriter writer = new BufferedWriter(new FileWriter("DatabaseUser.json"));
+        writer.write(new Gson().toJson(this.userDatabase));
         writer.close();
 
-        writer = new BufferedWriter(new FileWriter("src/database/DatabaseLeague.json"));
-        writer.write(new Gson().toJson(leagueDatabase));
+        writer = new BufferedWriter(new FileWriter("DatabaseLeague.json"));
+        writer.write(new Gson().toJson(this.leagueDatabase));
         writer.close();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,9 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        User user = Game.authenticateUser();
-        Game.start(user);
+        Game game = new Game();
+        User user = game.authenticateUser();
+        game.start(user);
     }
 }
