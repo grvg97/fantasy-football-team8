8dde517230925399e81520ad9f6bf66698030276
M.Cetin <m_cetin@m.cetin-Latitude-7490>
2020-03-12 12:24:45 +0100

    initial GUI commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
-import java.io.IOException;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Game game = new Game();
-        User user = game.authenticateUser();
-        game.start(user);
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        /*
+            Game game = new Game();
+            User user = game.authenticateUser();
+            game.start(user);
+        */
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
     }
 }
